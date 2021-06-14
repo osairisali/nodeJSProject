601cc3333a11e9e75d614be0797902ca71859386
ali akbar hakim <osairis.ali@gmail.com>
2021-06-14 13:17:25 +0700

    purging credentials

--- controllers/auth.js
+++ controllers/auth.js
@@ -17,8 +17,8 @@ const transporter = nodemailer.createTransport({
   host: "smtp.mailtrap.io",
   port: 2525,
   auth: {
-    user: `${process.env.NODEMAILE_USER}`,
-    pass: `${process.env.NODEMAILE_PASS}`,
+    user: `${process.env.NODEMAILER_USER}`,
+    pass: `${process.env.NODEMAILER_PASS}`,
   },
 });
 
